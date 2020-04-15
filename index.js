4f2669310c62532138af4d953d3fc799876a9c39
murali-sa <msintec@gmail.com>
2020-04-14 19:19:19 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,139 @@
+const cTable = require('console.table');
+const inquirer = require('inquirer');
+const mysql = require('mysql')
+//
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "devuser",
+    password: "AbcD123!",
+    database: "employeetracker_db"
+});
+//
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    // connection.end();
+});
+console.log("0 - Success!");
+//
+// command line input
+// prompt user - add or view or update or exit
+//
+init();
+//
+// start the program
+//
+function init() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "todo",
+            choices: [
+                "Add",
+                "View",
+                "Update",
+                "Exit"
+            ]
+        }
+    ]).then(function (data) {
+        var todo = data.todo;
+        console.log("To Do: ", todo);
+        console.log(data);
+        //
+        // switch based on what was chosen
+        //
+        switch (todo) {
+            case 'Add':
+                add();
+                break;
+            case 'View':
+                view();
+                break;
+            case 'Update':
+                update();
+                break;
+            case 'Exit':
+                console.log("Thank you.  Have a nice day.");
+                connection.end();
+                return;
+        }
+    });
+}
+//
+// function add - department, role, employee, or exit
+//
+function add() {
+    console.log("add chosen:");
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to ADD?",
+            name: "toAdd",
+            choices: [
+                "Department",
+                "Role",
+                "Employee",
+                "Go Back"
+            ]
+        }
+    ]).then(function (data) {
+        var toAdd = data.toAdd;
+        console.log("To Add: ", toAdd);
+        console.log(data);
+        //
+        // switch based on what was chosen
+        // add department, add role, add employee, or go back
+        //
+        switch (toAdd) {
+            case 'Department':
+                addDepartment();
+                break;
+            case 'Role':
+                addRole();
+                break;
+            case 'Update':
+                addEmployee();
+                break;
+            case 'Go Back':
+                init();
+                return;
+        }
+    });
+}
+//
+function addDepartment() {
+    console.log("add Department:");
+    inquirer
+        .prompt(
+            {
+                name: 'depName',
+                message: "Enter department name:",
+                type: 'input'
+            }
+        ).then(function (data) {
+            var depName = data.depName;
+            // should we check if the department name already exists?
+            //     connection.query(`INSERT INTO department (name) VALUES ('${depName}')`, function (err, data) {
+            //         if (err) throw err;
+            console.log(`Added`, depName)
+            //         getJob();
+        })
+}
+//
+function addRole() {
+    console.log("add Role:");
+}
+//
+function addemployee() {
+    console.log("add Employee:");
+}
+function view() {
+    console.log("view chosen:");
+}
+//
+function update() {
+    console.log("update chosen:");
+}
+//
